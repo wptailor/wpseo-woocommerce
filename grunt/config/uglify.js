01b01e51e94548fa8a4dc1b39b2793874f78a6bc
Danny <danny@yoast.com>
2015-11-04 13:52:01 +0100

    updated yaml file and added uglify.js

--- grunt/config/uglify.js
+++ grunt/config/uglify.js
@@ -1,1 +1,14 @@
-
+// https://github.com/gruntjs/grunt-contrib-uglify
+module.exports = {
+	"woo": {
+		options: {
+			preserveComments: "some",
+			report: "gzip"
+		},
+		files: {
+			"yoastseo-woo-plugin.min.js": [
+				"yoastseo-woo-plugin.js"
+			]
+		}
+	}
+};
